b8a7790f1401d17c97b60a182bd3b1e7df31ddbe
Beth Fitzgeralds <info@maggpress.com>
2016-11-17 11:15:13 -0500

    10-Directives-Part-2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
       var SongPlayer = {};
  /**
  * @function current album
@@ -22,6 +22,11 @@
 */
        
       SongPlayer.currentSong = null;
+       /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+      SongPlayer.currentTime = null;
 /**
  * @desc Buzz object audio file
  * @type {Object}
@@ -43,6 +48,12 @@
           formats: ['mp3'],
           preload: true
     });
+         
+         currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
  
         SongPlayer.currentSong = song;
  };
@@ -102,6 +113,16 @@
          playSong(song);
      }
  };  
+        /**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+      SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+     }
+ };
 /*
 @function playSong
 @desc Plays a song
@@ -118,5 +139,5 @@
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
